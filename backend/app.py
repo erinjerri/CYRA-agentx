e339dc4e0ca32b3dadf8c8ac6ff9d83873d8d13e
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2025-05-30 21:34:27 -0700

    updated app.py with flask and test image

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,32 @@
-# Flask backend placeholder
+# Flash backend
+
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.route("/analyze", methods=["GET", "POST"])
+def analyze():
+    print("Request.files:", request.files)
+    
+    screenshot = request.files.get("screenshot")
+
+    if screenshot:
+        print("Screenshot received:", screenshot.filename)
+    else:
+        print("No screenshot received.")
+
+    return jsonify({
+        "status": "success",
+        "message": "Files received",
+        "filename": screenshot.filename if screenshot else None
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5050)
+
